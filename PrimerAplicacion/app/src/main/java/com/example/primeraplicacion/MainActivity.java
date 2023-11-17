58c7ac8b47f05a70a563f0471b09a660e58b92f2
SrDamaez <david2013mip@gmail.com>
2023-11-17 10:39:46 -0500

    Preguntas

---
+++ PrimerAplicacion/app/src/main/java/com/example/primeraplicacion/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.primeraplicacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_responder;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        btn_responder=findViewById(R.id.btn_responder);
+
+    }
+}
