6c3881939610869df187d8008872d1d20dbb1518
SrDamaez <david2013mip@gmail.com>
2023-11-20 10:10:02 -0500

    xd

--- PrimerAplicacion/app/src/main/java/com/example/primeraplicacion/MainActivity1.java
+++ PrimerAplicacion/app/src/main/java/com/example/primeraplicacion/MainActivity1.java
@@ -2,14 +2,10 @@ package com.example.primeraplicacion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity1 extends AppCompatActivity {
 
     Button btn_responder;
 
@@ -17,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main1);
 
 
         btn_responder=findViewById(R.id.btn_responder);
